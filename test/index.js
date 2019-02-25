5889979c0647d6be7af2f754e68781bba69992de
gosadmin <327482475@qq.com>
2019-02-25 16:14:20 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var dsl2react = require('../index.js');
 
-var dsl = "<div amtype='NavBar' title='NavBar' mode='dark' rightIcon='ellipsis'><div amtype='Popover' id='popover2' data='popData'><div>popover</div></div></div>";
+var dsl = "<div data-type='NavBar' title='NavBar' mode='dark' rightIcon='ellipsis'><div data-type='Popover' id='popover2' data='popData'><div>popover</div></div></div>";
 
 var react = dsl2react(dsl, {});
 
